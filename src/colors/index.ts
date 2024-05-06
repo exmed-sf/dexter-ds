391b9a3856282f1f20245c53df6b7a2fdd535f53
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-06 09:58:32 -0300

    feat: add colors module

---
+++ src/colors/index.ts
@@ -0,0 +1,98 @@
+export const brandGreen = '#00d1bc';
+export const brandgrey = '#9f9f9f';
+
+/* Action color será a cor definida para indicar as interações na interface e é obrigatória. */
+
+export const brandButton = '#b1efe8';
+export const brandButtonFocus = '#89e9de';
+export const brandButtonHover = '#99eae1';
+export const brandButtonActive = '#26baa9';
+export const brandButtonDisabled = '#d0f5f1';
+
+/* empresas */
+
+export const empresasButtonFirst = '#6161ff';
+export const empresasButtonFirstFocus = '#5d5df6';
+export const empresasButtonFirstHover = '#5858ed';
+export const empresasButtonFirstActive = '#4646c2';
+export const empresasButtonFirstDisabled = '#d3d3fb';
+export const experimenteDisabled = '#ededfd';
+export const experimenteDisabledText = '#cccbcd';
+
+/* Neutral colors são as cores neutras, que geralmente compõem boa parte da interface.
+  A paleta neutra é usada em background, textos, estado normal de componentes, dentre outros. */
+
+export const neutralLightZero = '#ffffff';
+export const neutralLightOne = '#fafafa';
+export const neutralLightTwo = '#f7f7f7';
+export const neutralMidThree = '#e2e1e2';
+export const neutralMidFour = '#cccbcd';
+export const neutralMidFive = '#8a898e';
+export const neutralMidSix = '#747379';
+export const neutralDarkSeven = '#545356';
+export const neutralDarkEight = '#333333';
+export const neutralDarkNine = '#141414';
+
+/* Feedback colors, são cores geralmente usadas em mensagens de suporte ou respostas do sistema à ações realizadas. */
+
+export const feedbackPositive = '#107048';
+export const feedbackNegative = '#d46164';
+export const feedbackWorning = '##f9d349';
+export const feedbackInfo = '##23489f';
+
+/* Blue color será as cores definida para indicar as interações na interface relacionadas ao módulo seguro. */
+
+/* Seguro */
+
+export const lifePrimary = '#2684b5';
+export const lifePrimaryFocus = '#4e9ac2';
+export const lifePrimaryHover = '#3a8fbc';
+export const lifePrimaryActive = '#76b0cf';
+export const lifePrimaryDisabled = '#c5dce8';
+export const lifePrimaryHoverBanner = '#cfe1ec';
+export const blueDisabledLife = '#cfe1ec';
+export const lifePrimaryPressed = '#097AB5';
+
+/* Yellow color será as cores definida para indicar as interações na interface relacionadas ao módulo Rede Parceira. */
+/* Rede Parceira */
+export const rede = '#ffe999';
+export const redeFocus = '#f9e086';
+export const redeHover = '#fce590';
+export const redeActive = '#f2d773';
+export const redeDisabled = '#fbf2d3';
+export const redeBanner = '#c99e00';
+
+/* Red color será as cores definida para indicar as interações na interface relacionadas ao módulo Exmed Pass. */
+/* EXMED PASS */
+
+export const passPrimary = '#d46164';
+export const passPrimaryFocus = '#c1585b';
+export const passPrimaryHover = '#da6f72';
+export const passPrimaryActive = '#df7d7f';
+export const passPrimaryDisabled = '#ffcfd0';
+
+export const passSecond = '#ffcfd0';
+export const passSecondHover = '#ffffff';
+export const passSecondFocus = '#ffcfd0';
+export const passSecondActive = '#ffcfd0';
+export const passSecondDisabled = '#ffcfd0';
+
+/* Lilac color será as cores definida para complementar as interações na interface. */
+
+export const lilacDefault = '#8e95e6';
+export const lilacFocus = '#a5abeb';
+export const lilacHover = '#888edc';
+export const lilacPressed = '#8187d1';
+export const lilacDisabled = '#e8eafa';
+/* Orange color será as cores definida para complementar as interações na interface. */
+export const orangeDefault = '#ff8d12';
+export const orangeFocus = '#ff9727';
+export const orangeHover = '#ffa13b';
+export const orangePressed = '#e88010';
+export const orangeDisabled = '#ffdcb5';
+/* Green color será as cores definida para complementar as interações na interface. */
+export const greenDefault = '#26baa9';
+export const greenFocus = '#28c3b1';
+export const greenHover = '#2acbb8';
+export const greenPressed = '#1f988a';
+export const greenDisabled = '#a4dfda';
