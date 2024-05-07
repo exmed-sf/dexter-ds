59e0c759c3aa877bb956ced8e2164f21807aed97
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:47:30 -0300

    feat(colors): create lilac color

---
+++ src/colors/lilac.ts
@@ -0,0 +1,35 @@
+/**
+ * # Lilac colors
+ * Lilac color será as cores definida para complementar as interações na interface.
+ *
+ * ![lilac 00](https://dummyimage.com/48/8E95E6/000000?text=+) ![lilac 10](https://dummyimage.com/48/A5ABEB/000000?text=+) ![lilac 20](https://dummyimage.com/48/888EDC/000000?text=+) ![lilac 30](https://dummyimage.com/48/8187D1/000000?text=+) ![lilac 40](https://dummyimage.com/48/E8EAFA/000000?text=+)
+ */
+declare const lilac: {
+	/**
+	 * ### Lilac 0
+	 * ![lilac 00](https://dummyimage.com/48/8E95E6/000000?text=+)
+	 */
+	0: '#8E95E6';
+	/**
+	 * ### Lilac 10
+	 * ![lilac 10](https://dummyimage.com/48/A5ABEB/000000?text=+)
+	 */
+	10: '#A5ABEB';
+	/**
+	 * ### Lilac 20
+	 * ![lilac 20](https://dummyimage.com/48/888EDC/000000?text=+)
+	 */
+	20: '#888EDC';
+	/**
+	 * ### Lilac 30
+	 * ![lilac 30](https://dummyimage.com/48/8187D1/000000?text=+)
+	 */
+	30: '#8187D1';
+	/**
+	 * ### Lilac 40
+	 * ![lilac 40](https://dummyimage.com/48/E8EAFA/000000?text=+)
+	 */
+	40: '#E8EAFA';
+};
+
+export default lilac;
