a6b44a73e5646b0bf8dc48451b84469a9e130a33
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:46:51 -0300

    feat(colors): create brand color

---
+++ src/colors/brand.ts
@@ -0,0 +1,20 @@
+/**
+ * # Brand colors
+ * Brand colors são as cores que compõem a marca e a identidade visual. Além de poder ser usada como cor de ação, podem estar em interações com o logotipo, ilustrações, entre outras aplicações.
+ *
+ * ![brand 0](https://dummyimage.com/48/00D1BC/000000?text=+) ![brand 1](https://dummyimage.com/48/9F9F9F/000000?text=+)
+ */
+declare const brand: {
+	/**
+	 * ### Brand 0
+	 * ![brand 0](https://dummyimage.com/48/00D1BC/000000?text=+)
+	 */
+	0: '#00D1BC';
+	/**
+	 * ### Brand 1
+	 * ![brand 1](https://dummyimage.com/48/9F9F9F/000000?text=+)
+	 */
+	1: '#9F9F9F';
+};
+
+export default brand;
