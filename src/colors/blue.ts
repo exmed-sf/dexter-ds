a51387d0d6479e72491f6e58973e02bf434db12a
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:46:45 -0300

    feat(colors): create blue color

---
+++ src/colors/blue.ts
@@ -0,0 +1,35 @@
+/**
+ * # Blue colors
+ * Blue color será as cores definida para indicar as interações na interface relacionadas ao Exmed Life
+ *
+ * ![blue 00](https://dummyimage.com/48/2684B5/000000?text=+) ![blue 10](https://dummyimage.com/48/4E9AC2/000000?text=+) ![blue 20](https://dummyimage.com/48/3A8FBC/000000?text=+) ![blue 30](https://dummyimage.com/48/097AB5/000000?text=+) ![blue 40](https://dummyimage.com/48/C5DCE8/000000?text=+)
+ */
+declare const blue: {
+	/**
+	 * ### Blue 0
+	 * ![blue 00](https://dummyimage.com/48/2684B5/000000?text=+)
+	 */
+	0: '#2684B5';
+	/**
+	 * ### Blue 10
+	 * ![blue 10](https://dummyimage.com/48/4E9AC2/000000?text=+)
+	 */
+	10: '#4E9AC2';
+	/**
+	 * ### Blue 20
+	 * ![blue 20](https://dummyimage.com/48/3A8FBC/000000?text=+)
+	 */
+	20: '#3A8FBC';
+	/**
+	 * ### Blue 30
+	 * ![blue 30](https://dummyimage.com/48/097AB5/000000?text=+)
+	 */
+	30: '#097AB5';
+	/**
+	 * ### Blue 40
+	 * ![blue 40](https://dummyimage.com/48/C5DCE8/000000?text=+)
+	 */
+	40: '#C5DCE8';
+};
+
+export default blue;
