e3b0bf9339d08d800c137236cee6a8c66d9c89a1
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:47:03 -0300

    feat(colors): create feedback color

---
+++ src/colors/feedback.ts
@@ -0,0 +1,30 @@
+/**
+ * # Feedback colors
+ * Feedback colors, são cores geralmente usadas em mensagens de suporte ou respostas do sistema à ações realizadas.
+ *
+ * ![positive](https://dummyimage.com/48/107048/000000?text=+) ![negative](https://dummyimage.com/48/D46164/000000?text=+) ![warning](https://dummyimage.com/48/F9D349/000000?text=+) ![info](https://dummyimage.com/48/23489F/000000?text=+)
+ */
+declare const Feedback: {
+	/**
+	 * ### Positive
+	 * ![positive](https://dummyimage.com/48/107048/000000?text=+)
+	 */
+	positive: '#107048';
+	/**
+	 * ### Negative
+	 * ![negative](https://dummyimage.com/48/D46164/000000?text=+)
+	 */
+	negative: '#D46164';
+	/**
+	 * ### Warning
+	 * ![warning](https://dummyimage.com/48/F9D349/000000?text=+)
+	 */
+	warning: '#F9D349';
+	/**
+	 * ### Info
+	 * ![info](https://dummyimage.com/48/23489F/000000?text=+)
+	 */
+	info: '#23489F';
+};
+
+export default Feedback;
