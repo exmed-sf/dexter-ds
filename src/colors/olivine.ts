3ab1f31b9309e7ecfc4149a79ba7c01e368b8cdc
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:47:46 -0300

    feat(colors): create olivine color

---
+++ src/colors/olivine.ts
@@ -0,0 +1,35 @@
+/**
+ * # Olivine colors
+ * Olivine color será as cores definida para indicar as interações na interface relacionadas ao Exmed Care.
+ *
+ * ![olivine 00](https://dummyimage.com/48/AEC978/000000?text=+) ![olivine 10](https://dummyimage.com/48/9DBA63/000000?text=+) ![olivine 20](https://dummyimage.com/48/97B55C/000000?text=+) ![olivine 30](https://dummyimage.com/48/4E6A16/000000?text=+) ![olivine 40](https://dummyimage.com/48/B7C992/000000?text=+)
+ */
+declare const olivine: {
+	/**
+	 * ### Olivine 0
+	 * ![olivine 00](https://dummyimage.com/48/AEC978/000000?text=+)
+	 */
+	0: '#AEC978';
+	/**
+	 * ### Olivine 10
+	 * ![olivine 10](https://dummyimage.com/48/9DBA63/000000?text=+)
+	 */
+	10: '#9DBA63';
+	/**
+	 * ### Olivine 20
+	 * ![olivine 20](https://dummyimage.com/48/97B55C/000000?text=+)
+	 */
+	20: '#97B55C';
+	/**
+	 * ### Olivine 30
+	 * ![olivine 30](https://dummyimage.com/48/4E6A16/000000?text=+)
+	 */
+	30: '#4E6A16';
+	/**
+	 * ### Olivine 40
+	 * ![olivine 40](https://dummyimage.com/48/B7C992/000000?text=+)
+	 */
+	40: '#B7C992';
+};
+
+export default olivine;
