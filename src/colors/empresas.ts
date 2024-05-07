954f74777d7c01420cefa829c536e7251bd5a60a
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:46:57 -0300

    feat(colors): create empresas color

---
+++ src/colors/empresas.ts
@@ -0,0 +1,36 @@
+/**
+ * # Empresas colors
+ * Actions color será a cor definida para indicar as interações na interface e é obrigatória.
+ *
+ * ![empresas 00](https://dummyimage.com/48/6161FF/000000?text=+) ![empresas 10](https://dummyimage.com/48/5D5DF6/000000?text=+) ![empresas 20](https://dummyimage.com/48/5858ED/000000?text=+) ![empresas 30](https://dummyimage.com/48/4646C2/000000?text=+) ![empresas 40](https://dummyimage.com/48/D3D3FB/000000?text=+)
+ */
+
+declare const empresas: {
+	/**
+	 * ### Empresas 0
+	 * ![empresas 00](https://dummyimage.com/48/6161FF/000000?text=+)
+	 */
+	0: '#6161FF';
+	/**
+	 * ### Empresas 10
+	 * ![empresas 10](https://dummyimage.com/48/5D5DF6/000000?text=+)
+	 */
+	10: '#5D5DF6';
+	/**
+	 * ### Empresas 20
+	 * ![empresas 20](https://dummyimage.com/48/5858ED/000000?text=+)
+	 */
+	20: '#5858ED';
+	/**
+	 * ### Empresas 30
+	 * ![empresas 30](https://dummyimage.com/48/4646C2/000000?text=+)
+	 */
+	30: '#4646C2';
+	/**
+	 * ### Empresas 40
+	 * ![empresas 40](https://dummyimage.com/48/D3D3FB/000000?text=+)
+	 */
+	40: '#D3D3FB';
+};
+
+export default empresas;
