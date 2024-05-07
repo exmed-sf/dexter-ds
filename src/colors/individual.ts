a5f4edb253f77cf941d462ea81e890773cddf40d
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:47:23 -0300

    feat(colors): create individual color

---
+++ src/colors/individual.ts
@@ -0,0 +1,35 @@
+/**
+ * # Individual colors
+ * Actions color será a cor definida para indicar as interações na interface e é obrigatória.
+ *
+ * ![individual 00](https://dummyimage.com/48/B1EFE8/000000?text=+) ![individual 10](https://dummyimage.com/48/89E9DE/000000?text=+) ![individual 20](https://dummyimage.com/48/99EAE1/000000?text=+) ![individual 30](https://dummyimage.com/48/26BAA9/000000?text=+) ![individual 40](https://dummyimage.com/48/D0F5F1/000000?text=+)
+ */
+declare const individual: {
+	/**
+	 * ### Individual 0
+	 * ![individual 00](https://dummyimage.com/48/B1EFE8/000000?text=+)
+	 */
+	0: '#B1EFE8';
+	/**
+	 * ### Individual 10
+	 * ![individual 10](https://dummyimage.com/48/89E9DE/000000?text=+)
+	 */
+	10: '#89E9DE';
+	/**
+	 * ### Individual 20
+	 * ![individual 20](https://dummyimage.com/48/99EAE1/000000?text=+)
+	 */
+	20: '#99EAE1';
+	/**
+	 * ### Individual 30
+	 * ![individual 30](https://dummyimage.com/48/26BAA9/000000?text=+)
+	 */
+	30: '#26BAA9';
+	/**
+	 * ### Individual 40
+	 * ![individual 40](https://dummyimage.com/48/D0F5F1/000000?text=+)
+	 */
+	40: '#D0F5F1';
+};
+
+export default individual;
