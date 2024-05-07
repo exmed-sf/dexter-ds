d722091b268258fec3dd170aa879b7a0550deb64
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:47:59 -0300

    feat(colors): create red color

---
+++ src/colors/red.ts
@@ -0,0 +1,35 @@
+/**
+ * # Red colors
+ * Red color será as cores definida para indicar as interações na interface relacionadas ao Exmed Pass.
+ *
+ * ![red 00](https://dummyimage.com/48/D46164/000000?text=+) ![red 10](https://dummyimage.com/48/DF7D7F/000000?text=+) ![red 20](https://dummyimage.com/48/D55D61/000000?text=+) ![red 30](https://dummyimage.com/48/C1585B/000000?text=+) ![red 40](https://dummyimage.com/48/FFCFD0/000000?text=+)
+ */
+declare const red: {
+	/**
+	 * ### Red 0
+	 * ![red 00](https://dummyimage.com/48/D46164/000000?text=+)
+	 */
+	0: '#D46164';
+	/**
+	 * ### Red 10
+	 * ![red 10](https://dummyimage.com/48/DF7D7F/000000?text=+)
+	 */
+	10: '#DF7D7F';
+	/**
+	 * ### Red 20
+	 * ![red 20](https://dummyimage.com/48/D55D61/000000?text=+)
+	 */
+	20: '#D55D61';
+	/**
+	 * ### Red 30
+	 * ![red 30](https://dummyimage.com/48/C1585B/000000?text=+)
+	 */
+	30: '#C1585B';
+	/**
+	 * ### Red 40
+	 * ![red 40](https://dummyimage.com/48/FFCFD0/000000?text=+)
+	 */
+	40: '#FFCFD0';
+};
+
+export default red;
