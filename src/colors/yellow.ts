4619e12f9f63e6b3588dcf8f8af89f64d9a69eed
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2024-05-07 14:48:06 -0300

    feat(colors): create yellow color

---
+++ src/colors/yellow.ts
@@ -0,0 +1,35 @@
+/**
+ * # Yellow colors
+ * Yellow color será as cores definida para indicar as interações na interface relacionadas a Rede Parceira.
+ *
+ * ![yellow 00](https://dummyimage.com/48/FFE999/000000?text=+) ![yellow 10](https://dummyimage.com/48/F9E086/000000?text=+) ![yellow 20](https://dummyimage.com/48/FCE590/000000?text=+) ![yellow 30](https://dummyimage.com/48/F2D773/000000?text=+) ![yellow 40](https://dummyimage.com/48/FBF2D3/000000?text=+)
+ */
+declare const yellow: {
+	/**
+	 * ### Yellow 0
+	 * ![yellow 00](https://dummyimage.com/48/FFE999/000000?text=+)
+	 */
+	0: '#FFE999';
+	/**
+	 * ### Yellow 10
+	 * ![yellow 10](https://dummyimage.com/48/F9E086/000000?text=+)
+	 */
+	10: '#F9E086';
+	/**
+	 * ### Yellow 20
+	 * ![yellow 20](https://dummyimage.com/48/FCE590/000000?text=+)
+	 */
+	20: '#FCE590';
+	/**
+	 * ### Yellow 30
+	 * ![yellow 30](https://dummyimage.com/48/F2D773/000000?text=+)
+	 */
+	30: '#F2D773';
+	/**
+	 * ### Yellow 40
+	 * ![yellow 40](https://dummyimage.com/48/FBF2D3/000000?text=+)
+	 */
+	40: '#FBF2D3';
+};
+
+export default yellow;
